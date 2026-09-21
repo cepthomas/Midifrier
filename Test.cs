023859a1c9c294e8ef00442eaf4e115d9a010c62
cepthomas <cepthomas@gmail.com>
2026-09-21 10:40:51 -0500

    Global update and clean up

--- Test.cs
+++ Test.cs
@@ -6,36 +6,34 @@ using System.Linq;
 using Ephemera.NBagOfTricks;
 using Ephemera.NBagOfTricks.PNUT;
 using Midifrier;
+using System.Windows.Forms;
 
-// Some test code, ignore.
-// Useful files - from https://github.com/cepthomas/TestAudioFiles:
-// Style file, full info: _LoveSong.S474.sty
-// Plain midi, full song: WICKGAME.MID (has other stuff after the last track)
-// Plain midi, one instrument, no patch: bass_ch2.mid
-// Plain midi, drums on different channel: _drums_ch1.mid
+// Some test code, you can ignore.
+// It uses files from https://github.com/cepthomas/TestAudioFiles:
+//  - Style file, full info: _LoveSong.S474.sty
+//  - Plain midi, full song: WICKGAME.MID (has other stuff after the last track)
+//  - Plain midi, one instrument, no patch: bass_ch2.mid
+//  - Plain midi, drums on different channel: _drums_ch1.mid
 
 
 namespace Midifrier.Test
 {
     //----------------------------------------------------------------
-    public static class Executor
+    public static class Common
     {
-        // This needs DEV_PATH set, or hack to taste.
-        public static string InputDir = Path.Join(Environment.GetEnvironmentVariable("DEV_PATH"), "Misc", "TestAudioFiles");
-        public static string OutputDir = Path.Join(MiscUtils.GetSourcePath(), "out");
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible", Justification = "<Pending>")]
+        public static string InputDir;
+        public static string OutputDir;
 
-        /// <summary>Test entry.</summary>
-        public static void Run()
+        static Common()
         {
-            // Ensure paths.
-            Directory.CreateDirectory(OutputDir);
-
-            TestRunner runner = new(OutputFormat.Readable);
-            //  MLEX_SIMPLE  MLEX_STYLE  MLEX_DUMP  MLEX
-            var torun = new[] { "MLEX_DUMP" };
-            runner.RunSuites(torun);
-
-            // File.WriteAllLines(Path.Join(MiscUtils.GetSourcePath(), "test.txt"), runner.Context.OutputLines);
+            InputDir = Path.Join(Environment.GetEnvironmentVariable("DEV_PATH"), "Misc", "TestAudioFiles");
+            if (!Path.Exists(InputDir))
+            {
+                MessageBox.Show($"Test files path {InputDir} is not valid.{Environment.NewLine}This needs DEV_PATH set, or hack source to taste.");
+                Environment.Exit(1);
+            }
+            OutputDir = Path.Join(MiscUtils.GetSourcePath(), "out");
         }
     }
 
@@ -48,7 +46,7 @@ namespace Midifrier.Test
             StopOnFail(true);
 
             MidiDataFile mfd = new();
-            mfd.Read(Path.Join(Executor.InputDir, "WICKGAME.MID"));
+            mfd.Read(Path.Join(Common.InputDir, "WICKGAME.MID"));
 
             //var numtr = mfd!.NumTracks; // 10
             var pnames = mfd.GetPatternNames();
@@ -61,13 +59,13 @@ namespace Midifrier.Test
 
                var hdr = mfd.Header;
 
-               var fn1 = Path.Join(Executor.OutputDir, "simple_midi_all");
+               var fn1 = Path.Join(Common.OutputDir, "simple_midi_all");
                List<int> chs1 = [];
                MidiExport.ExportCsv($"{fn1}.csv", pattern, chs1, hdr);
                MidiExport.ExportMidi($"{fn1}.mid", pattern, chs1, hdr);
                MidiExport.ExportText($"{fn1}.txt", pattern, chs1, hdr);
 
-               var fn2 = Path.Join(Executor.OutputDir, "simple_midi_some");
+               var fn2 = Path.Join(Common.OutputDir, "simple_midi_some");
                List<int> chs2 = [1, 2, 3];
                MidiExport.ExportCsv($"{fn2}.csv", pattern, chs2, hdr);
                MidiExport.ExportMidi($"{fn2}.mid", pattern, chs2, hdr);
@@ -86,7 +84,7 @@ namespace Midifrier.Test
 
             // Style file, full info:
             var mfd = new MidiDataFile();
-            mfd.Read(Path.Join(Executor.InputDir, "_LoveSong.S474.sty"));
+            mfd.Read(Path.Join(Common.InputDir, "_LoveSong.S474.sty"));
             Assert(mfd is not null);
 
             // Load the new one.
@@ -101,7 +99,7 @@ namespace Midifrier.Test
 
                 var hdr = mfd.Header;
 
-                var fn1 = Path.Join(Executor.OutputDir, "style_Main_C");
+                var fn1 = Path.Join(Common.OutputDir, "style_Main_C");
                 List<int> chs1 = [];
                 MidiExport.ExportCsv($"{fn1}.csv", pattern, chs1, hdr);
                 MidiExport.ExportMidi($"{fn1}.mid", pattern, chs1, hdr);
@@ -119,14 +117,14 @@ namespace Midifrier.Test
             StopOnFail(true);
 
             // Style file, full info:
-            var fnsimple = Path.Join(Executor.InputDir, "WICKGAME.MID");
-            var fnstyle = Path.Join(Executor.InputDir, "_LoveSong.S474.sty");
+            var fnsimple = Path.Join(Common.InputDir, "WICKGAME.MID");
+            var fnstyle = Path.Join(Common.InputDir, "_LoveSong.S474.sty");
 
             var t = MidiExport.Dump(fnsimple, true);
-            File.WriteAllLines(Path.Join(Executor.OutputDir, "_dump_simple.txt"), t);
+            File.WriteAllLines(Path.Join(Common.OutputDir, "_dump_simple.txt"), t);
 
             t = MidiExport.Dump(fnstyle, true);
-            File.WriteAllLines(Path.Join(Executor.OutputDir, "_dump_style.txt"), t);
+            File.WriteAllLines(Path.Join(Common.OutputDir, "_dump_style.txt"), t);
         }
     }
 }
